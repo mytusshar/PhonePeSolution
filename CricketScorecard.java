b7d8d82a9d84a56e694be39d2b4a5edfddadac12
tusharbochare <tushar.bochare@sarvaha.com>
2021-01-31 15:46:27 +0530

    Refactoring

--- CricketScorecard.java
+++ CricketScorecard.java
@@ -1,23 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/* 
-input2:
-5 2
-P1 P2 P3 P4 P5
-B1 B2 B3 B4 B5
-
-P6 P7 P8 P9 P10
-B6 B7 B8 B9 B10
-
-1 1 1 1 1 2
-W 4 4 Wd W 1 6
-
-4 6 W W 1 1
-6 1 W W
-
-*/
-
 public class CricketScorecard {
     private ArrayList<Team> teams;
     private Team team1;
